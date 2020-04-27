2f7ca0bca773fa1c160e078eec6923f012d4e7cd
bumbot <pkim1295@gmail.com>
2020-04-27 16:47:39 -0400

    added create route in clinics controller

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -10,4 +10,30 @@ class ClinicsController < ApplicationController
 
         render json: clinic, except: [:created_at, :updated_at]
     end
+
+    def create
+        clinic = Clinic.find_or_create_by(
+            name: params["name"],
+            address: params["address"],
+            city: params["city"],
+            state: params["state"],
+            zip: params["zip"],
+            address_id: params["address_id"],
+            phone_number: params["phone_number"],
+            website_url: params["website_url"],
+            latitude: params["latitude"],
+            longitude: params["longitude"],
+            new_patients: params["new_patients"]
+        )
+        if clinic
+            render json: {
+                error: false
+            }
+        else
+            render json: {
+                message: `unable to add clinic number: #{clinic.address_id}`,
+                error: true
+            }
+        end
+    end
 end
